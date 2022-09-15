cf05b41429c4f7d5610fee3c04bb2d4bad5679ca
yavuz360fabriek <yavuzkaand@hotmail.com>
2022-09-15 13:06:05 +0200

    Typo and routing issue fixed

--- client/src/components/NavRouter.tsx
+++ client/src/components/NavRouter.tsx
@@ -2,18 +2,18 @@ import {Routes, Route} from "react-router-dom";
 import {Home} from "../pages/Home/index";
 import {AddProduct} from "../pages/CreateProduct/index";
 import {Task} from "../pages/Task/index";
-import {Login} from "../pages/auth/login/index";
-import { Product } from "../pages/Product/index";
-import { Register } from "../pages/auth/register/index";
+import {Login} from "../pages/Auth/login/index";
+import { Products } from "../pages/Product/index";
+import { Register } from "../pages/Auth/register/index";
 
 const NavRouter = () => {
     return(
         <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/products" element={<AddProduct />} />
+            <Route path="/products" element={<Products />} />
             <Route path="/tasks" element={<Task />} />
             <Route path="/login" element={<Login />} />
-            <Route path="/addproduct" element={<Product />} />
+            <Route path="/addproduct" element={<AddProduct />} />
             <Route path="/signup" element={<Register />} />
         </Routes>
     )
--- client/src/pages/Product/index.tsx
+++ client/src/pages/Product/index.tsx
@@ -1,155 +1,88 @@
-import { useState } from "react"
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
-import {toast} from "react-hot-toast";
-import "./index.css";
+import { useState, useEffect } from "react";
 
-const Product = () => {
-  const navigate = useNavigate();
+const Products = () => {
+    const [itemproducts, setItemProducts] = useState<any>();
 
-  interface TProduct {
-    name: string;
-    description: string;
-    category: string;
-    price: number;
-    img_url: string;
-  }
+    useEffect(() => {
+        axios.get('http://localhost:3001/items')
+            .then(res => {
+                setItemProducts(res.data);
+            }).catch(err => {
+                console.log(err);
+            })
+    }, [])
 
-  // const productData = { name: '', category: '', description: '', price: 0, img_url: ''};
-  // const [product, setProduct] = useState<{name:string, description:string, category:string, price:number, img_url:string}>();
-  const [product, setProduct] = useState<Partial<TProduct>>({ name: '', description: '', category: '', price: 0, img_url: '' });
+    return (
+        // <p>{JSON.stringify(products)}</p>
+        <>
 
-  const addItem = () => {
-    axios.post('http://localhost:3001/createitem', product).then(response => {
+            {/* <div className="flex flex-col items-end  mt-3">
+                <button className="bg-purple-700 hover:bg-purple-900 text-white px-6 py-2 text-md rounded-md mr-2 ">Add Product</button>
+            </div> */}
+            <div className="overflow-x-auto relative flex justify-center items-center mt-2">
+                <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                    <thead className="text-xs text-gray-700 uppercase bg-dark-900 dark:bg-gray-900 dark:text-gray-400">
+                        <tr>
+                            <th scope="col" className="py-3 px-6">
+                                Product name
+                            </th>
+                            <th scope="col" className="py-3 px-6">
+                                Category
+                            </th>
+                            <th scope="col" className="py-3 px-6">
+                                Description
+                            </th>
+                            <th scope="col" className="py-3 px-6">
+                                Price
+                            </th>
+                            <th scope="col" className="py-3 px-6">
 
-      if (response.status === 201) {
-        toast.success("Item has been created");
-        navigate("/products");
-      } else {
-        console.log(response);
-        toast.error(response.data);
-        
-      }
-    })
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
 
-  }
+                        {itemproducts?.map((item: { name: string; category: string; description: string; price: number; img_url: string; }) => {
+                            return (
+                                <tr key={item.name} className="bg-white border-b dark:bg-gray-50 dark:border-gray-50">
+                                    <th scope="row" className="py-4 px-6 whitespace-nowrap">
+                                        {item.name}
+                                    </th>
+                                    <td className="py-4 px-6">
+                                        {item.category}
+                                    </td>
+                                    <td className="py-4 px-6">
+                                        {item.description}
+                                    </td>
+                                    <td className="py-4 px-6">
+                                        {item.price}
+                                    </td>
+                                    <td className="py px-6">
+                                        <button style={{ "cursor": "pointer" }}>
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" /></svg>
+                                        </button>
 
-  return (
-    <>
-    
-      <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-        <div className="w-full max-w-md space-y-8">
-          <div>
-            <img
-              className="mx-auto h-12 w-auto"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-              alt="Your Company"
-            />
-            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-              Add Item
-            </h2>
-          </div>
-          <form className="mt-8 space-y-6" action="#" method="POST">
-            <input type="hidden" name="remember" defaultValue="true" />
-            <div className="-space-y-px rounded-md shadow-sm">
-              <div>
-                <label htmlFor="text" className="sr-only">
-                  product name
-                </label>
-                <input
-                  id="product-name"
-                  name="product-name"
-                  type="text"
+                                        <button style={{ "cursor": "pointer" }}>
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                                            </svg>
 
-                  required
-                  className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                  placeholder="Product name"
-                  value={product?.name}
-                  onChange={e => setProduct({ ...product, name: e.target.value })}
-                />
-              </div>
-              <div className="pt-3">
+                                        </button>
 
-                <select onChange={e => setProduct({ ...product, category: e.target.value })} defaultValue={product?.category} id="countries" className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm">
-                  <option value={""}>Choose a category</option>
-                  <option value="Tech">Tech</option>
-                  <option value="Food">Food</option>
-                  <option value="Games">Games</option>
-                  <option value="Clothes">Clothes</option>
 
-                </select>
-              </div>
-              <div className="pt-3">
-                <label htmlFor="text" className="sr-only">
-                  Description
-                </label>
-                <input
-                  id="description"
-                  name="description"
-                  type="text"
+                                    </td>
 
-                  required
-                  className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                  placeholder="Description"
-
-                  value={product?.description}
-                  onChange={e => setProduct({ ...product, description: e.target.value })}
-                />
-              </div>
-              <div className="pt-3">
-                <label htmlFor="text" className="sr-only">
-                  Price
-                </label>
-                <input
-                  id="price"
-                  name="price"
-                  type="number"
-
-                  required
-                  className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                  placeholder="Price"
-                  value={product?.price}
-                  onChange={e => setProduct({ ...product, price: parseInt(e.target.value) })}
-                />
-              </div>
-              <div className="pt-3">
-                <label htmlFor="text" className="sr-only">
-                  img_url
-                </label>
-                <input
-                  id="img_url"
-                  name="img_url"
-                  type="text"
-
-                  required
-                  className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                  placeholder="Img_url"
-
-                  value={product?.img_url}
-                  onChange={e => setProduct({ ...product, img_url: e.target.value })}
-                />
-              </div>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
             </div>
-            <div>
-              <button
-                onClick={(e) => {
-                  e.preventDefault();
-                  addItem()
-                }}
-                className="mt-2 group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-              >
-                <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-                  {/* <LockClosedIcon className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" /> */}
-                </span>
-                Add Item
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>
-    </>
-  )
+        </>
+    )
 }
 
 
-export { Product }
+export { Products }
